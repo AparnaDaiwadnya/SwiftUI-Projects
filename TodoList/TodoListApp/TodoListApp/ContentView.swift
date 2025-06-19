5ef4e08fbb0446ba90e82eaefe3758d59be15ae7
Aparna Daiwadnya <aparnawadivkar@gmail.com>
2025-06-19 15:59:49 +0530

    Initialize TodoListApp SwiftUI project
    Set up a new Xcode project for TodoListApp with basic SwiftUI structure, including ContentView, main app entry point, and default asset catalogs.

---
+++ TodoList/TodoListApp/TodoListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoListApp
+//
+//  Created by Anuja Deshmukh Patil on 19/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList/TodoListApp/TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Anuja Deshmukh Patil on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
