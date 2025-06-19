2b01e4d0f198d98126260e7c324f48f92dd173f7
Aparna Daiwadnya <aparnawadivkar@gmail.com>
2025-06-19 16:02:24 +0530

    Initialize HelloSwiftUIApp SwiftUI project
    Add initial Xcode project files, asset catalogs, and basic SwiftUI implementation with ContentView and main app entry point.

---
+++ HelloSwiftUI/HelloSwiftUIApp/HelloSwiftUIApp/HelloSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIAppApp.swift
+//  HelloSwiftUIApp
+//
+//  Created by Anuja Deshmukh Patil on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
